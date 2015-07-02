237f040f9884a6947f53120a4ee8b1642b52a71e
Victor Souza <victor@localhost.localdomain>
2015-07-02 01:22:29 -0300

    api

---
+++ api.py
@@ -0,0 +1,21 @@
+import get_access_token
+from requests_oauthlib import OAuth1
+from status_update import status_up
+
+
+consumer_key = "IF5yN6xFTqJSLzqpH8AsOgo0y"
+consumer_secret = "SeUCPcF1g6P3vrArhQZDS1Q849id3VwequIwbp4PdiqUouqfwl"
+
+token, secret = get_access_token.main(consumer_key, consumer_secret)
+
+def get_oauth(token, secret):
+    oauth = OAuth1(consumer_key,
+                client_secret=consumer_secret,
+                resource_owner_key=token,
+                resource_owner_secret=secret)
+    return oauth
+
+oauth = get_oauth(token,secret)
+status = "testando api do twitter  com #python"
+up_status = status_up(oauth, status)
+print up_status.json()
